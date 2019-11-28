ce8d3fb47b9016414c52b29dc23b36afbde5d4c6
Giovanni Mogna <mogna.giovanni@gmail.com>
2019-11-28 10:00:25 +0100

    Created custom version of react scripts

--- packages/react-scripts/scripts/utils/verifyTypeScriptSetup.js
+++ packages/react-scripts/scripts/utils/verifyTypeScriptSetup.js
@@ -256,7 +256,7 @@ function verifyTypeScriptSetup() {
   if (!fs.existsSync(paths.appTypeDeclarations)) {
     fs.writeFileSync(
       paths.appTypeDeclarations,
-      `/// <reference types="react-scripts" />${os.EOL}`
+      `/// <reference types="rankout-react-scripts" />${os.EOL}`
     );
   }
 }
